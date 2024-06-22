a1eaa57abbf6966cc6fd4501793b992cef097f6b
AdiJT <aditaklal@gmail.com>
2024-06-22 22:13:56 +0800

    Perbaiki link di navbar

---
+++ PKMGerejaEbenhaezer.Web/Areas/Dashboard/Controllers/PengumumanController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PKMGerejaEbenhaezer.Web.Areas.Dashboard.Controllers
+{
+    public class PengumumanController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
