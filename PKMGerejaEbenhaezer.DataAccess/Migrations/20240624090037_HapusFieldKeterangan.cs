8f41ad771a3a6115db853ffce0d29d17b39eb38e
AdiJT <aditaklal@gmail.com>
2024-06-24 17:27:53 +0800

    Perbaiki seeding data Rayon

---
+++ PKMGerejaEbenhaezer.DataAccess/Migrations/20240624090037_HapusFieldKeterangan.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PKMGerejaEbenhaezer.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class HapusFieldKeterangan : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Keterangan",
+                table: "PengumumanTable");
+
+            migrationBuilder.UpdateData(
+                table: "AppUserTable",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Password",
+                value: "AQAAAAIAAYagAAAAEJBpJCCz0zrrlyTixy9omix+q7be0l2Lchp8gXJkk4iBxL+6L8+hUVnpSfA15iYgSw==");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Keterangan",
+                table: "PengumumanTable",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.UpdateData(
+                table: "AppUserTable",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Password",
+                value: "AQAAAAIAAYagAAAAEDM79towDCHnVNcUtz3mMs7wRkXlJFaok8QOHUAShqW+5GapRFwyKKxOPluqvITdBw==");
+        }
+    }
+}
---
+++ PKMGerejaEbenhaezer.DataAccess/Migrations/20240624092400_SeedDataRayoPerbaiki.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PKMGerejaEbenhaezer.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedDataRayoPerbaiki : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "AppUserTable",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Password",
+                value: "AQAAAAIAAYagAAAAEEbmtTQA3F+QSLVxUPgRYNWzrDXtiS5AwIp9Yz3uvfcYYZeGhwgeqzCWQcxQwFi8Cg==");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "AppUserTable",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "Password",
+                value: "AQAAAAIAAYagAAAAEG6agSyM9+GBTflNDgC/zb5++gPleEsrQ0lk+b0Pzh+utVusXKFX7i1ImbmrBXejfg==");
+        }
+    }
+}
