c61e7f8abcb780dd0c7470cc8617216931e3b1bb
AdiJT <aditaklal@gmail.com>
2024-06-26 07:18:11 +0800

    Tambah tipe chart

--- PKMGerejaEbenhaezer.Web/Models/ChartModel.cs
+++ PKMGerejaEbenhaezer.Web/Models/ChartModel.cs
@@ -2,7 +2,31 @@
 {
     public class ChartModel
     {
-        public Dictionary<string, double> Data { get; set; }
-        public string Label { get; set; }
+        public Dictionary<string, double> Data { get; set; } = new Dictionary<string, double>();
+        public string Label { get; set; } = string.Empty;
+        public ChartType Type { get; set; }
+    }
+
+    public enum ChartType
+    {
+        Doughnut, Bar, PolarArea
+    }
+
+    public static class ChartTypeExtension
+    {
+        public static string ToStringJS(this ChartType chartType)
+        {
+            switch (chartType)
+            {
+                case ChartType.Doughnut:
+                    return "doughnut";
+                case ChartType.Bar:
+                    return "bar";
+                case ChartType.PolarArea:
+                    return "polarArea";
+            };
+
+            return string.Empty;
+        }
     }
 }
