d70f570fd13d8bdd59bc4da9c381b363cf4cc4f8
AdiJT <aditaklal@gmail.com>
2024-06-30 17:43:30 +0800

    Tambah halaman index dan detail pengumuman

--- PKMGerejaEbenhaezer.Web/Controllers/PengumumanController.cs
+++ PKMGerejaEbenhaezer.Web/Controllers/PengumumanController.cs
@@ -13,6 +13,47 @@ namespace PKMGerejaEbenhaezer.Web.Controllers
             _appDbContext = appDbContext;
         }
 
+        public enum Kategori
+        {
+            Semua, Bulan
+        }
+
+        public async Task<IActionResult> Index(Kategori kategori = Kategori.Semua, int bulan = 1)
+        {
+            var daftarPengumuman = _appDbContext.PengumumanTable
+                .Include(p => p.Foto)
+                .Include(p => p.Pembuat)
+                .AsNoTracking();
+
+            if(kategori == Kategori.Bulan)
+            {
+                daftarPengumuman = daftarPengumuman.Where(p => p.TanggalDiBuat.Month == bulan)
+                    .OrderByDescending(p => p.TanggalDiBuat);
+
+                var modelPerbulan = await daftarPengumuman.ToListAsync();
+
+                return View(modelPerbulan);
+            }
+
+            var model = await daftarPengumuman
+                .OrderByDescending(p => p.TanggalDiBuat).ToListAsync();
+
+            return View(model);
+        }
+
+        public async Task<IActionResult> Detail(int id)
+        {
+            var pengumuman = await _appDbContext.PengumumanTable.Where(p => p.Id == id)
+                .Include(p => p.Foto)
+                .Include(p => p.Pembuat)
+                .FirstOrDefaultAsync();
+
+            if(pengumuman == null)
+                return NotFound();
+
+            return View(pengumuman);
+        }
+
         public async Task<IActionResult> Dokumen(int id)
         {
             var pengumuman = await _appDbContext.PengumumanTable.Where(p => p.Id == id)
