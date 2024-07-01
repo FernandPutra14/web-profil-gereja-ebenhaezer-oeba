ff889d9e4ea40fedae1661eb1ba66ef34ce9a587
putra <fernandputra14@gmail.com>
2024-07-02 00:58:47 +0800

    Tambah Desain Warta jemaat, pendeta, dan beberapa lainnya

--- PKMGerejaEbenhaezer.Web/wwwroot/js/carousel.js
+++ PKMGerejaEbenhaezer.Web/wwwroot/js/carousel.js
@@ -2,18 +2,73 @@
 $('.fernand').owlCarousel({
     loop: true,
     autoplay: true,
-    autoplayTimeout: 4000,
+    autoplayTimeout: 8000,
     margin: 10,
     nav: true,
+    navText: [
+        "<i class='ri-arrow-left-s-fill'></i>",
+        "<i class='ri-arrow-right-s-fill'></i>"
+    ],
     responsive: {
         0: {
             items: 1
         },
         600: {
-            items: 1
+            items: 2
         },
         1000: {
-            items: 3
+            items: 4
         }
     }
 });
+
+$(document).ready(function () {
+    function setCarousel() {
+        if ($(window).width() >= 1200) {
+            $('.fajar').owlCarousel({
+                loop: false,
+                autoplay: false,
+                margin: 10,
+                nav: false,
+                items: 3,
+                mouseDrag: false,
+                touchDrag: false
+            });
+        } else {
+            $('.fajar').owlCarousel({
+                loop: true,
+                autoplay: false,
+                autoplayTimeout: 8000,
+                margin: 10,
+                nav: false,
+                navText: [
+                    "<i class='ri-arrow-left-s-fill'></i>",
+                    "<i class='ri-arrow-right-s-fill'></i>"
+                ],
+                responsive: {
+                    0: {
+                        items: 1
+                    },
+                    750: {
+                        items: 2
+                    },
+                    1200: {
+                        items: 2
+                    },
+                }
+            });
+        }
+    }
+
+    setCarousel();
+
+    $(window).resize(function () {
+        $('.fajar').trigger('destroy.owl.carousel');
+        $('.fajar').find('.owl-stage-outer').children().unwrap();
+        $('.fajar').removeClass("owl-center owl-loaded owl-text-select-on");
+
+        setCarousel();
+    });
+});
+
+
---
+++ PKMGerejaEbenhaezer.Web/wwwroot/js/parallax.js
@@ -0,0 +1,19 @@
+﻿function adjustBackgroundPosition() {
+    var scrollPosition = window.pageYOffset;
+    var elements = ['hero'];
+
+    elements.forEach(function (id) {
+        var element = document.getElementById(id);
+        if (element) {
+            element.style.backgroundPositionY = -(scrollPosition * 0.5) + 'px';
+        }
+    });
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    adjustBackgroundPosition();
+});
+
+document.addEventListener('scroll', function () {
+    adjustBackgroundPosition();
+});
