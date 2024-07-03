e2afbc48b548942b9eb8bb89367709ec68b77a5e
putra <fernandputra14@gmail.com>
2024-07-03 15:03:09 +0800

    Buat Navbar Fixed on Scroll after hero section

--- PKMGerejaEbenhaezer.Web/wwwroot/js/site.js
+++ PKMGerejaEbenhaezer.Web/wwwroot/js/site.js
@@ -1,1 +1,17 @@
-﻿
+﻿const nav_header = document.querySelector(".head");
+const section_hero = document.querySelector("#hero");
+
+
+const observer = new IntersectionObserver(
+    (entries) => {
+        const ent = entries[0];
+        console.log(ent);
+        ent.isIntersecting == false ? nav_header.classList.add("stick") :
+            nav_header.classList.remove("stick");
+}, {
+    root: null,
+    rootMargin: "",
+    threshold: "",
+});
+
+observer.observe(section_hero);
