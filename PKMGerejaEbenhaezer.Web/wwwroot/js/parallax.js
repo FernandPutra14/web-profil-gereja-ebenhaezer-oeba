5c5b915e50368077475f2598be6772af01f1caf5
putra <fernandputra14@gmail.com>
2024-07-06 18:42:28 +0800

    Bismilah 2

--- PKMGerejaEbenhaezer.Web/wwwroot/js/parallax.js
+++ PKMGerejaEbenhaezer.Web/wwwroot/js/parallax.js
@@ -10,10 +10,28 @@
     });
 }
 
+function saveScrollPosition() {
+    sessionStorage.setItem('scrollPosition', window.pageYOffset);
+}
+
+function restoreScrollPosition() {
+    var scrollPosition = sessionStorage.getItem('scrollPosition');
+    if (scrollPosition !== null) {
+        window.scrollTo(0, parseInt(scrollPosition));
+    }
+}
+
 document.addEventListener('DOMContentLoaded', function () {
+    if (performance.navigation.type === performance.navigation.TYPE_RELOAD) {
+        restoreScrollPosition();
+    }
     adjustBackgroundPosition();
 });
 
 document.addEventListener('scroll', function () {
     adjustBackgroundPosition();
+});
+
+window.addEventListener('beforeunload', function () {
+    saveScrollPosition();
 });
