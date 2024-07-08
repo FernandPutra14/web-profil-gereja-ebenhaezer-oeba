8e5eb52466b286c7517ec5cfd24e1fe5d42e7358
AdiJT <aditaklal@gmail.com>
2024-07-08 14:14:29 +0800

    Selesai buat ibadah di dashboard

--- PKMGerejaEbenhaezer.Web/Areas/Dashboard/Controllers/IbadahController.cs
+++ PKMGerejaEbenhaezer.Web/Areas/Dashboard/Controllers/IbadahController.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using PKMGerejaEbenhaezer.DataAccess.Data;
+using PKMGerejaEbenhaezer.Domain.Entity;
+using PKMGerejaEbenhaezer.Web.Areas.Dashboard.Models.Ibadah;
 
 namespace PKMGerejaEbenhaezer.Web.Areas.Dashboard.Controllers
 {
@@ -15,6 +18,63 @@ namespace PKMGerejaEbenhaezer.Web.Areas.Dashboard.Controllers
         {
             _appDbContext = appDbContext;
             _logger = logger;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var daftarIbadah = await _appDbContext.IbadahTable
+                .Include(i => i.KategoriIbadah)
+                .Include(i => i.Pendeta)
+                .AsNoTracking().ToListAsync();
+
+            return View(daftarIbadah);
+        }
+
+        //Tambah Ibadah
+        public IActionResult Tambah()
+        {
+            return View(new TambahVM());
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Tambah(TambahVM tambahVM)
+        {
+            //Validasi
+            if (!ModelState.IsValid) return View(tambahVM);
+
+            //Simpan ke database
+            var kategori = await _appDbContext.KategoriIbadahTable
+                .Where(k => k.Id == tambahVM.IdKategoriIbadah).FirstOrDefaultAsync();
+
+            var pendeta = await _appDbContext.PendetaTable
+                .Where(p => p.Id == tambahVM.IdPendeta).FirstOrDefaultAsync();
+
+            var ibadah = new Ibadah
+            {
+                Id = 0,
+                Judul = tambahVM.Judul,
+                Deskripsi = tambahVM.Deskripsi,
+                NasPembimbing = tambahVM.NasPembimbing,
+                TanggalIbadah = tambahVM.TanggalIbadah,
+                Tempat = tambahVM.Tempat,
+                KategoriIbadah = kategori,
+                Pendeta = pendeta,
+            };
+
+            _appDbContext.IbadahTable.Add(ibadah);
+
+            try
+            {
+                await _appDbContext.SaveChangesAsync();
+            }
+            catch(Exception ex)
+            {
+                ModelState.AddModelError(string.Empty, "Simpan gagal. Laporkan error ke administrator");
+                _logger.LogError("Tambah. Simpan Gagal. Exception {0}", ex.ToString());
+                return View(tambahVM);
+            }
+
+            return RedirectToAction(nameof(Index));
         }
     }
 }
---
+++ PKMGerejaEbenhaezer.Web/Areas/Dashboard/Models/Ibadah/TambahVM.cs
@@ -0,0 +1,36 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PKMGerejaEbenhaezer.Web.Areas.Dashboard.Models.Ibadah
+{
+    public class TambahVM
+    {
+        [Display(Name = "Judul")]
+        [Required(ErrorMessage = "{0} harus diisi")]
+        public string Judul { get; set; } = string.Empty;
+
+        [Display(Name = "Deskripsi")]
+        [Required(ErrorMessage = "{0} harus diisi")]
+        public string Deskripsi { get; set; } = string.Empty;
+
+        [Display(Name = "Nas Pembimbing")]
+        [Required(ErrorMessage = "{0} harus diisi")]
+        public string NasPembimbing { get; set; } = string.Empty;
+
+        [Display(Name = "Tempat")]
+        [Required(ErrorMessage = "{0} harus diisi")]
+        public string Tempat { get; set; } = string.Empty;
+
+        [DataType(DataType.DateTime)]
+        [Display(Name = "Tanggal Ibadah")]
+        [Required(ErrorMessage = "{0} harus diisi")]
+        public DateTime TanggalIbadah { get; set; }
+
+        [Display(Name = "Kategori Ibadah")]
+        [Required(ErrorMessage = "{0} harus diisi")]
+        public int IdKategoriIbadah { get; set; }
+
+        [Display(Name = "Pendeta")]
+        [Required(ErrorMessage = "{0} harus diisi")]
+        public int IdPendeta { get; set; }
+    }
+}
