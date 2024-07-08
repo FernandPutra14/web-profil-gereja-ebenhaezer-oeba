8c14948cd34c9ec41fae0ca567cfa5ec3a2ee051
AdiJT <aditaklal@gmail.com>
2024-07-08 20:44:38 +0800

    Tambah toastr.js

---
+++ PKMGerejaEbenhaezer.Web/Services/ToastrNotification/ToastrNotification.cs
@@ -0,0 +1,15 @@
+﻿namespace PKMGerejaEbenhaezer.Web.Services.ToastrNotification
+{
+    public class ToastrNotification
+    {
+        public ToastrNotificationType Type { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Message { get; set; } = string.Empty;
+        public TimeSpan Duration { get; set; }
+    }
+
+    public enum ToastrNotificationType
+    {
+        Success, Warning, Error, Info
+    }
+}
