419d959b4cbdcf0f3b5b522b6d904e170f01a15d
AdiJT <aditaklal@gmail.com>
2024-07-10 12:10:11 +0800

    Tambah View WartaJemaat dan Ibadah

---
+++ PKMGerejaEbenhaezer.Web/Controllers/IbadahController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PKMGerejaEbenhaezer.Web.Controllers
+{
+    public class IbadahController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ PKMGerejaEbenhaezer.Web/Controllers/WartaJemaatController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PKMGerejaEbenhaezer.Web.Controllers
+{
+    public class WartaJemaatController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
