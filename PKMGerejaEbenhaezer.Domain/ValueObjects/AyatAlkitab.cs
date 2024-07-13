a8c2e9e971627452411aea66a1860803fa6e05e7
AdiJT <aditaklal@gmail.com>
2024-07-13 13:36:53 +0800

    Selesai buat tipe ayat alkitab dan tesnya

---
+++ PKMGerejaEbenhaezer.Domain/ValueObjects/AyatAlkitab.cs
@@ -0,0 +1,219 @@
+﻿using Humanizer;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PKMGerejaEbenhaezer.Domain.ValueObjects
+{
+    public class AyatAlkitab : IEquatable<AyatAlkitab>, IParsable<AyatAlkitab>
+    {
+        public Kitab Kitab { get; private set; }
+        public int Pasal { get; private set; }
+        public int[] Ayat { get; private set; } = Array.Empty<int>();
+
+        private AyatAlkitab()
+        {
+
+        }
+
+        public AyatAlkitab(Kitab kitab, int pasal, int[] ayat)
+        {
+            Kitab = kitab;
+            Pasal = pasal;
+            Ayat = ayat;
+        }
+
+        public override string ToString()
+        {
+            var ayatString = "";
+
+            if(Ayat.Length > 0)
+            {
+                ayatString = ":";
+                if(Ayat.Length == 2)
+                {
+                    ayatString += $"{Ayat[0]}-{Ayat[1]}";
+                }
+                else
+                {
+                    ayatString += string.Join(",", Ayat);
+                }
+            }
+
+            return $"{Kitab.Humanize()} {Pasal}{ayatString}";
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj is null) return false;
+
+            if (obj is not AyatAlkitab other) return false;
+
+            return other.Kitab == Kitab && other.Pasal == Pasal && other.Ayat.SequenceEqual(Ayat);
+        }
+
+        public bool Equals(AyatAlkitab? other)
+        {
+            return other is not null &&
+                other.Kitab == Kitab && other.Pasal == Pasal && other.Ayat.SequenceEqual(Ayat);
+        }
+
+        public static bool operator==(AyatAlkitab a, AyatAlkitab b)
+        {
+            return a.Equals(b); 
+        }
+
+        public static bool operator!=(AyatAlkitab a, AyatAlkitab b)
+        {
+            return !(a == b);
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Kitab.GetHashCode(), Pasal.GetHashCode(), Ayat.GetHashCode());
+        }
+
+        //Format : {Kitab} {Pasal}:{ayat}
+        //Format : {Kitab} {Pasal}:{ayat}-{ayat}
+        //Format : {Kitab} {Pasal}:{ayat},{ayat},{ayat}
+        public static AyatAlkitab Parse(string s, IFormatProvider? provider)
+        {
+            var kitabCharArray = s.TakeWhile(c => c != ':');
+            var kitabString = string
+                .Join("", kitabCharArray.Reverse().SkipWhile(c => int.TryParse($"{c}", out _)).Reverse())
+                .Trim();
+            var kitab = kitabString.DehumanizeTo<Kitab>();
+
+            var splits = s.Remove(0, kitabString.Length).Split(":");
+            if (splits.Length != 2 && splits.Length != 1)
+                throw new FormatException();
+
+            var pasalString = splits[0];
+            var pasal = int.Parse(pasalString);
+
+            var ayat = Array.Empty<int>();
+
+            if(splits.Length == 2)
+            {
+                var ayatsString = splits[1];
+                
+                if (ayatsString.Contains('-'))
+                {
+                    var ayatsStringSplit = ayatsString.Split('-');
+
+                    if(ayatsStringSplit.Length != 2)
+                        throw new FormatException();
+
+                    ayat = ayatsStringSplit.Select(c => int.Parse(c)).ToArray();
+                }
+                else
+                {
+                    var ayatsStringSplit = ayatsString.Split(',');
+
+                    ayat = ayatsStringSplit.Select(c => int.Parse(c)).ToArray();
+                }
+            }
+
+            return new AyatAlkitab(kitab, pasal, ayat);
+        }
+
+        public static bool TryParse([NotNullWhen(true)] string? s, IFormatProvider? provider, [MaybeNullWhen(false)] out AyatAlkitab result)
+        {
+            try
+            {
+                var ayatAlkitab = Parse(s!, provider);
+                result = ayatAlkitab;
+                return true;
+            }
+            catch(Exception)
+            {
+                result = null;
+                return false;
+            }
+        }
+    }
+
+    public enum Kitab
+    { 
+        Kejadian = 1, Keluaran, Imamat, Bilangan, Ulangan, Yosua,
+
+        [Display(Name = "Hakim-hakim", Description = "Hakim-hakim")]
+        HakimHakim,
+
+        Rut,
+
+        [Display(Name = "1 Samuel", Description = "1 Samuel")]
+        Samuel1,
+
+        [Display(Name = "2 Samuel", Description = "2 Samuel")]
+        Samuel2,
+
+        [Display(Name = "1 Raja-Raja", Description = "1 Raja-Raja")]
+        RajaRaja1,
+
+        [Display(Name = "2 Raja-Raja", Description = "2 Raja-Raja")]
+        RajaRaja2,
+
+        [Display(Name = "1 Tawarikh", Description = "1 Tawarikh")]
+        Tawarikh1,
+
+        [Display(Name = "2 Tawarikh", Description = "2 Tawarikh")]
+        Tawarikh2,
+
+        Ezra, Nehemia, Ester, Ayub, Mazmur, Pengkhotbah,
+
+        [Display(Name = "Kidung Agung", Description = "Kidung Agung")]
+        KidungAgung,
+
+        Yesaya, Yeremia, Ratapan, Yehezkiel, Daniel, Hosea, Yoel, Amos, Obaja, Yunus, Mikha,
+        Nahum, Habakuk, Zefanya, Hagai, Zakharia, Maleakhi, Matius, Markus, Lukas, Yohanes,
+
+        [Display(Name = "Kisah Para Rasul", Description = "Kisah Para Rasul")]
+        KisahParaRasul,
+
+        Roma,
+
+        [Display(Name = "1 Korintus", Description = "1 Korintus")]
+        Korintus1,
+
+        [Display(Name = "2 Korintus", Description = "2 Korintus")]
+        Korintus2,
+
+        Galatia, Efesus, Filipi, Kolose,
+
+        [Display(Name = "1 Tesalonika", Description = "1 Tesalonika")]
+        Tesalonika1,
+
+        [Display(Name = "2 Tesalonika", Description = "2 Tesalonika")]
+        Tesalonika2,
+
+        [Display(Name = "1 Timotius", Description = "1 Timotius")]
+        Timotius1,
+
+        [Display(Name = "2 Timotius", Description = "2 Timotius")]
+        Timotius2,
+
+        Titus, Filemon, Ibrani, Yakobus,
+
+        [Display(Name = "1 Petrus", Description = "1 Petrus")]
+        Petrus1,
+
+        [Display(Name = "2 Petrus", Description = "2 Petrus")]
+        Petrus2,
+
+        [Display(Name = "1 Yohanes", Description = "1 Yohanes")]
+        Yohanes1,
+
+        [Display(Name = "2 Yohanes", Description = "2 Yohanes")]
+        Yohanes2,
+
+        [Display(Name = "3 Yohanes", Description = "3 Yohanes")]
+        Yohanes3,
+
+        Yudas, Wahyu
+    }
+}
---
+++ PKMGerejaEbenhaezer.Test/AyatAlkitabTests.cs
@@ -0,0 +1,126 @@
+﻿using PKMGerejaEbenhaezer.Domain.ValueObjects;
+
+namespace PKMGerejaEbenhaezer.Test
+{
+    public class AyatAlkitabTests
+    {
+        [ClassData(typeof(AyatAlkitabTestData))]
+        [Theory]
+        public void ToString_Should_EqualToValueString(AyatAlkitab value, string valueString)
+        {
+            //Act
+            var s = value.ToString();
+
+            //Assert
+            Assert.Equal(valueString, s);
+        }
+
+        [ClassData(typeof(AyatAlkitabTestData))]
+        [Theory]
+        public void TryParse_Should_ReturnTrueAndResultEqualToValue(AyatAlkitab value, string valueString)
+        {
+            //Act
+            var isValid = AyatAlkitab.TryParse(valueString, null, out AyatAlkitab? result);
+
+            //Assert
+            Assert.True(isValid);
+            Assert.NotNull(result);
+            Assert.Equal(value, result);
+        }
+
+        [ClassData(typeof(AyatAlkitabEqualTestData))]
+        [Theory]
+        public void Equals_Should_ReturnTrue(AyatAlkitab value1, AyatAlkitab value2)
+        {
+            //Act
+            var result = value1.Equals(value2);
+
+            //Assert
+            Assert.Equal(value1, value2);
+            Assert.True(result);
+        }
+
+        [ClassData(typeof(AyatAlkitabEqualTestData))]
+        [Theory]
+        public void EqualOperator_Should_ReturnTrue(AyatAlkitab value1, AyatAlkitab value2)
+        {
+            //Act
+            var result = value1 == value2;
+
+            //Assert
+            Assert.Equal(value1, value2);
+            Assert.True(result);
+        }
+
+        [ClassData(typeof(AyatAlkitabNotEqualTestData))]
+        [Theory]
+        public void Equals_Should_ReturnFalse(AyatAlkitab value1, AyatAlkitab value2)
+        {
+            //Act
+            var result = value1.Equals(value2);
+
+            //Assert
+            Assert.NotEqual(value1, value2);
+            Assert.False(result);
+        }
+
+        [ClassData(typeof(AyatAlkitabNotEqualTestData))]
+        [Theory]
+        public void EqualOperator_Should_ReturnFalse(AyatAlkitab value1, AyatAlkitab value2)
+        {
+            //Act
+            var result = value1 == value2;
+
+            //Assert
+            Assert.NotEqual(value1, value2);
+            Assert.False(result);
+        }
+    }
+
+    public class AyatAlkitabTestData : TheoryData<AyatAlkitab, string>
+    {
+        public AyatAlkitabTestData()
+        {
+            Add(new AyatAlkitab(Kitab.Kejadian, 2, new int[] { 2 }), "Kejadian 2:2");
+            Add(new AyatAlkitab(Kitab.Kejadian, 2, new int[] { 2, 5 }), "Kejadian 2:2-5");
+            Add(new AyatAlkitab(Kitab.Kejadian, 2, new int[] { 2, 5, 7 }), "Kejadian 2:2,5,7");
+            Add(new AyatAlkitab(Kitab.Samuel1, 12, new int[] { 1 }), "1 Samuel 12:1");
+            Add(new AyatAlkitab(Kitab.Samuel2, 12, Array.Empty<int>()), "2 Samuel 12");
+        }
+    }
+
+    public class AyatAlkitabEqualTestData : TheoryData<AyatAlkitab, AyatAlkitab>
+    {
+        public AyatAlkitabEqualTestData()
+        {
+            foreach(Kitab item in Enum.GetValues(typeof(Kitab)))
+            {
+                var ayat1 = new AyatAlkitab(item, 1, Array.Empty<int>());
+                var ayat2 = new AyatAlkitab(item, 12, new int[] { 1, 100 });
+                var ayat3 = new AyatAlkitab(item, 123, new int[] { 2, 5, 6, 8 });
+
+                Add(ayat1, ayat1);
+                Add(ayat2, ayat2);
+                Add(ayat3, ayat3);
+            }
+        }
+    }
+
+    public class AyatAlkitabNotEqualTestData : TheoryData<AyatAlkitab, AyatAlkitab>
+    {
+        public AyatAlkitabNotEqualTestData()
+        {
+            foreach (Kitab item in Enum.GetValues(typeof(Kitab)))
+            {
+                var ayat1 = new AyatAlkitab(item, 1, Array.Empty<int>());
+                var ayat2 = new AyatAlkitab(item, 12, new int[] { 1, 100 });
+                var ayat3 = new AyatAlkitab(item, 123, new int[] { 2, 5, 6, 8 });
+
+                Add(ayat1, ayat2);
+                Add(ayat1, ayat3);
+                Add(ayat2, ayat3);
+                Add(ayat3, ayat1);
+            }
+        }
+    }
+}
