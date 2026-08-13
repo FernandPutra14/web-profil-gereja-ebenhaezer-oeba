20de580d1ace4bd0895cd0e37d9b44c910e85a79
AdiJT <aditaklal@gmail.com>
2026-08-13 23:28:57 +0800

    Sedikit

--- PKMGerejaEbenhaezer.Web/Program.cs
+++ PKMGerejaEbenhaezer.Web/Program.cs
@@ -133,8 +133,10 @@ app.MapControllerRoute(
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
 
-using var scope = app.Services.CreateScope();
-var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-db.Database.Migrate();
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
 
 app.Run();
